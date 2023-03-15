ec0b4221a695d14a9aaef0338816eea54c3f7a58
Chetan <chetanpaliwalxyz@gmail.com>
2023-03-15 15:46:16 +0530

    push

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask, app, render_template, request
 import pandas as pd
+
 app = Flask(__name__)
 
-
 @app.route("/")
+@app.route('/index')
 def index():
     return render_template('index.html')
 
@@ -13,4 +14,7 @@ def data():
     df = pd.read_excel(excelData)
     print(df)
     return "success"
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
 
