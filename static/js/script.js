d89552920acab5abad27006b8fd749e6018d7d02
Chetan <chetanpaliwalxyz@gmail.com>
2023-03-19 03:58:43 +0530

    added reset and download func

--- static/js/script.js
+++ static/js/script.js
@@ -17,3 +17,38 @@ function data() {
         }
     });
 }
+
+var doc = new jsPDF();
+var specialElementHandlers = {
+    '#editor': function (element, renderer) {
+        return true;
+    }
+};
+
+$('#cmd').click(function () {
+    doc.fromHTML($('#content').html(), 15, 15, {
+        'width': 800,
+            'elementHandlers': specialElementHandlers
+    },
+    function(bla) {
+        const today = new Date();
+        name = today.toLocaleString(); // 5/12/2020, 6:50:21 PM
+        doc.save(name+'.pdf');
+    });
+    // setTimeout(function () {
+    //     doc.save(name+'.pdf');
+    // }, 5);
+
+    // doc.save(name+'.pdf');
+    // var element = document.getElementById('content');
+    // element.style.width = '800px';
+    // element.style.height = '1000px';
+    // var opt = {
+    //     margin: 0.5,
+    //     filename: 'sample.pdf',
+    //     image: {type: 'jpeg', quality: 1},
+    //     html2canvas: {scale: 1},
+    //     jspdf: {unit: 'in', format: 'letter', orientation: 'portrait', precision: '12'}
+    // };
+    // html2pdf().set(opt).from(element).save();
+});
